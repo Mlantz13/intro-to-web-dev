f4d99c4d0afcee57a494ab5e240fd7735bcba317
Mlantz13 <mllantz@millersville.edu>
2022-03-19 23:47:05 -0400

    Week 5 stuff

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -6,14 +6,14 @@ function submitAnswerOne() {
   // Get the text from answer one's input element
   const answerOne = answerOneInput.value;
 
-  if(answerOne == 'cat') {
-    alert("That's right!");
+  if(answerOne == 'Pong') {
+    alert("That's Right, Good Job!");
     hide('question-one');
     show('question-two');
-  } else if(answerOne == 'dog') {
-    alert('Close, but my favorite animal runs away from dogs.');
+  } else if(answerOne == 'Computer Space') {
+    alert('Close, go a few years back.');
   } else {
-    alert('Wrong! Hint: My favorite animal meows.')
+    alert('Wrong! Hint:It was created in 1958 by William Higinbotham .')
   }
 }
 
@@ -25,17 +25,83 @@ function submitAnswerTwo() {
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
-    alert("That's right!");
+  if(answerTwo == 'Minecraft') {
+    alert("You're Killing it!");
     hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
+    show('question-three');
+  } else if(answerTwo == 'Tetris') {
+    alert('almost, think of a more modern blocky game.');
+  } else if(answerTwo == 'GTAV') {
+    alert("Not that one, but its up there");
   } else {
-    alert('The answer is lower.')
+    alert("Incorrect, think of a blocky game")
   }
 }
 
+function submitAnswerThree() {
+
+  const answerThreeInput = document.getElementById('answer-three-input');
+
+  const answerThree = answerThreeInput.value;
+
+  if(answerThree == 'Playstation 2' ) {
+    alert("Great Job!");
+    hide('question-three');
+    show('question-four')
+  } else if(answerThree == 'Nintendo DS') {
+    alert("That's the second most sold console, try again!");
+  } else if(answerThree == 'Xbox 360') {
+    alert("Not quite, try it's competitor");
+  } else if(answerThree == 'Playstation 3') {
+    alert("Same family, try a different generation");
+  } else {
+    alert("Good guess, but not that one")
+  }
+}
+
+function submitAnswerFour() {
+
+  const answerFourInput = document.getElementById('answer-four-input');
+
+  const answerFour = answerFourInput.value;
+
+  if(answerFour == 'Pokemon') {
+    alert("Almost There!");
+    hide('question-four');
+    show('question-five');
+  } else if(answerFour == 'Super Mario') {
+    alert("Close but not quite, think little monsters battling each other")
+  } else if(answerFour == 'Call of Duty') {
+    alert("Not that one, think a bit more kid friendly")
+  } else if(answerFour == 'The Elder Scrolls') {
+    alert("No, but a good franchise still!")
+  } else if(answerFour == 'Sonic') {
+    alert("Woah, slow down there!")
+  } else {
+    alert("Incorrect, hint: it also has a popular tv show")
+  }
+}
+
+function submitAnswerFive() {
+
+  const answerFiveInput = document.getElementById('answer-five-input');
+
+  const answerFive = answerFiveInput.value;
+
+  if(answerFive == "Hell Yeah") {
+    alert("That's What I'm talking about!");
+    hide('question-five');
+    show('done');
+  } else if(answerFive == 'yes') {
+    alert("I think you mis-spelled Hell Yeah?")
+  } else if(answerFive == 'maybe') {
+    alert("What's that supposed to mean?")
+  } else if(answerFive == 'no') {
+    alert("I'm gonna need you to exit this page now")
+  } else {
+    alert("Dude, come on it's a yes or no question")
+  }
+}
 // Helper function that takes an ID and shows the element with that ID.
 function show(id) {
   const element = document.getElementById(id);
