d1220778f57b69e76a4633d4fb1002c6d6c845e1
Mlantz13 <mllantz@millersville.edu>
2022-03-27 23:27:31 -0400

    Update script.js

--- week-08-objects/script.js
+++ week-08-objects/script.js
@@ -1,27 +1,39 @@
 // Array of objects representing a todo list.
 // Modify this array to contain your own list.
-const taskArray = [
-  {label: 'Water plants', time: 3},
-  {label: 'Homework', time: 2},
-  {label: 'Laundry', time: 1},
+const cartoonArray = [
+  {title: 'Chowder', aired: 2007, seasons: 2},
+  {title: 'Regular Show', aired: 2009, seasons: 8},
+  {title: 'Adventure Time', aired: 2010, seasons: 9},
+  {title: 'Courage the Cowardly Dog', aired: 1996, seasons: 4},
+  {title: 'Steven Universe', aired:2013, seasons: 5},
+  {title: 'The Grim Adventures of Billy and Mandy', aired: 2001, seasons: 6},
+  {title: 'Codename: Kids Next Door', aired: 2002, seasons: 6},
+  {title: 'We Bare Bears', aired: 2015, seasons: 4},
+  {title: 'Samurai Jack', aired: 2001, seasons: 5},
+  {title: 'Teen Titans', aired: 2003, seasons: 5},
+  {title: 'Fosters Home for Imaginary Friends', aired: 2004, seasons: 6},
+  {title: 'Star Wars: The Clone Wars', aired: 2008, seasons: 7},
+  {title: 'Ben 10', aired: 2005, seasons: 4},
+  {title: 'The Amazing World of Gumball', aired: 2011, seasons: 6},
 ];
 
 // Loads the content into the page.
 function loadContent() {
   // This line of code sorts the array alphabetically by the task labels.
   // Modify this to sort your data by a different field, or just delete it.
-  taskArray.sort((a, b) => compare(a.label, b.label));
+  cartoonArray.sort((a, b) => compare(a.title, b.title));
 
   loadTable();
-  loadShortestTask();
+  loadEarliestAirTime();
 }
 
 // Adds a task to the array and reloads the page content.
-function addNewTask() {
-  const newTaskLabel = document.getElementById('label-input').value;
-  const newTaskTime = document.getElementById('time-input').value;
-  const newTask = {label: newTaskLabel, time: newTaskTime };
-  taskArray.push(newTask);
+function addNewCartoon() {
+  const newCartoonTitle = document.getElementById('title-input').value;
+  const newCartoonAirdate = document.getElementById('aired-input').value;
+  const newCartoonSeasons = document.getElementById('seasons-input').value;
+  const newTask = {title: newCartoonTitle, aired: newAirdate, seasons: newCartoonSeasons};
+  cartoonArray.push(newCartoon);
 
   loadContent();
 }
